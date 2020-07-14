91eb08897926a408364b7fd1776ffa65d1d35566
ruth <ruth@192.168.1.123>
2020-07-14 17:51:40 +0100

    Try multiple configuration.

--- custom_components/multi_mycroft/__init__.py
+++ custom_components/multi_mycroft/__init__.py
@@ -3,21 +3,21 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_IP_ADDRESS
+from homeassistant.const import CONF_NAME, CONF_IP_ADDRESS
 from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "multi_mycroft"
-
 CONFIG_SCHEMA = vol.Schema(
-    {DOMAIN: vol.Schema({vol.Required(CONF_IP_ADDRESS): cv.string})}, extra=vol.ALLOW_EXTRA
+    {vol.Required(CONF_NAME): cv.string, vol.Required(CONF_IP_ADDRESS): cv.string},
+    extra=vol.ALLOW_EXTRA,
 )
 
-
 def setup(hass, config):
-    """Set up the Multi Mycroft component."""
-    hass.data[DOMAIN] = config[DOMAIN][IP_ADDRESS]
+    return True
+
+def setup_entry(hass, entry):
+    hass.data[DOMAIN] = config[DOMAIN][CONF_NAME][CONF_IP_ADDRESS]
     discovery.load_platform(hass, "notify", DOMAIN, {}, config)
     return True
