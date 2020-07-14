4b1c043f4fe766b3ab917f85532dd57428898962
ruth <ruth@192.168.1.123>
2020-07-14 17:21:57 +0100

    Add setup function.

--- custom_components/multi_mycroft/__init__.py
+++ custom_components/multi_mycroft/__init__.py
@@ -1,1 +1,23 @@
 """The Multiple Mycroft component."""
+import logging
+
+import voluptuous as vol
+
+from homeassistant.const import IP_ADDRESS
+from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "multi_mycroft"
+
+CONFIG_SCHEMA = vol.Schema(
+    {DOMAIN: vol.Schema({vol.Required(IP_ADDRESS): cv.string})}, extra=vol.ALLOW_EXTRA
+)
+
+
+def setup(hass, config):
+    """Set up the Multi Mycroft component."""
+    hass.data[DOMAIN] = config[DOMAIN][IP_ADDRESS]
+    discovery.load_platform(hass, "notify", DOMAIN, {}, config)
+    return True
